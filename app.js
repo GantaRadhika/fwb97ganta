aedb2d494d028767700755d09830ada68463db8c
GantaRadhika <89550564+GantaRadhika@users.noreply.github.com>
2021-11-03 18:42:08 -0500

    added code

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var EggfryRouter = require('./routes/Eggfry');
 var addmodsRouter = require('./routes/addmods');
+var slectorRouter = require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -24,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Eggfry', EggfryRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', slectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
